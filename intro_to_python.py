e172dc876242225d5a9fea8d21f190e8adb3aeb6
gcreneti <122646969+gcreneti@users.noreply.github.com>
2023-01-24 12:59:29 -0500

    Delete intro_to_python.py

--- intro_to_python.py
+++
@@ -1,9 +0,0 @@
-import numpy as numpy
-mat1 = numpy.array([[1,0,0], [0,1,0], [0,0,1]]) 
-print(mat1)
-print("\n")
-mat2 = numpy.array([[1,3,3],[3,1,3],[3,3,1]])
-print(mat2)
-print("\n")
-mat3 = numpy.array([[1,3],[3,1],[3,3]])
-print(mat3)
